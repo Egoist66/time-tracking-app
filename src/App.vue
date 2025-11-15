31032169b40e1973dc7b314293cd161df0a6a015
Farid Mahmudov <Razormad666@gmail.com>
2025-11-15 19:56:19 +0300

    Implement loading state in App component: Add Loader component and utilize Suspense for route transitions, enhancing user experience during page loading.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 import AppLayout from "@/components/layout/AppLayout.vue";
+import Loader from "@/components/ui/loader/Loader.vue";
+
 </script>
 
 <template>
   <AppLayout>
-    <RouterView v-slot="{ Component }">
-      <transition name="route" mode="out-in">
-        <component :is="Component" />
-      </transition>
+    <RouterView v-slot="{ Component, route }">
+      <Suspense>
+        <template v-if="route.name !== 'not-found'" #default>
+          <transition name="route" mode="out-in">
+            <component :is="Component" />
+          </transition>
+        </template>
+        <template v-else #default>
+          <component :is="Component" />
+        </template>
+        <template #fallback>
+          <Loader />
+        </template>
+      </Suspense>
     </RouterView>
   </AppLayout>
 </template>
---
+++ src/components/ui/loader/Loader.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col items-center justify-center h-screen gap-4">
+    <div
+      class="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-indigo-600"
+    ></div>
+    <p class="text-lg font-medium text-gray-600 dark:text-gray-400">
+      Loading...
+    </p>
+  </div>
+</template>
+
