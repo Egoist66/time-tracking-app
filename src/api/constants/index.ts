d855be996180668d70fc1912c2e4366e6206a974
Farid Mahmudov <Razormad666@gmail.com>
2025-11-22 23:19:25 +0300

    fix: добавлен пустой перенос строки в файл констант API
    - Добавлен пустой перенос строки в `src/api/constants/index.ts` для улучшения читаемости кода.

--- src/api/constants/index.ts
+++ src/api/constants/index.ts
@@ -8,6 +8,7 @@ export const redirectUri = import.meta.env.VITE_ASANA_REDIRECT_URI;
 export const asanaToken = import.meta.env.VITE_ASANA_TOKEN;
 
 
+
 // Asana constants
 export const asanaBaseUrl = "https://app.asana.com/api/1.0";
 export const asanaState =  Math.random().toString(36).substring(2, 15);
