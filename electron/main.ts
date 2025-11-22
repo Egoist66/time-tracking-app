7eab9aa5c6f40f886e25f1124c363e09c89d57c5
Farid Mahmudov <Razormad666@gmail.com>
2025-11-22 20:53:54 +0300

    feat: добавлена поддержка Electron и обновлены зависимости
    - Добавлен новый плагин `vite-plugin-electron` для интеграции с Electron.
    - Обновлен `package.json` с добавлением скрипта сборки для Electron и указанием главного файла.
    - Обновлен `.gitignore` для исключения новых директорий `dist-electron` и `release`.
    - Обновлен `vite.config.ts` для настройки плагина Electron с указанием входных файлов.

---
+++ electron/main.ts
@@ -0,0 +1,58 @@
+import { app, BrowserWindow } from 'electron'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+// Require to work with ES modules
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+// Process env variables
+process.env.DIST = path.join(__dirname, '../dist')
+process.env.VITE_PUBLIC = app.isPackaged ? process.env.DIST : path.join(process.env.DIST, '../public')
+
+let win: BrowserWindow | null
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.mjs'),
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  })
+
+  // Test active push message to Renderer-process.
+  win.webContents.on('did-finish-load', () => {
+    win?.webContents.send('main-process-message', (new Date).toLocaleString())
+  })
+
+  if (process.env.VITE_DEV_SERVER_URL) {
+    win.loadURL(process.env.VITE_DEV_SERVER_URL)
+    win.webContents.openDevTools()
+  } else {
+    // win.loadFile('dist/index.html')
+    win.loadFile(path.join(process.env.DIST, 'index.html'))
+  }
+}
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
+app.whenReady().then(createWindow)
+
--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,28 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import path from 'path'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import electron from 'vite-plugin-electron/simple'
 
 export default defineConfig({
-  plugins: [vue({
-    features: {
-      propsDestructure: true,
-      customElement: true,
-    },
-  }), tailwindcss(), tsconfigPaths(), vueDevTools()],
+  plugins: [
+    vue({
+      features: {
+        propsDestructure: true,
+        customElement: true,
+      },
+    }),
+    tailwindcss(),
+    tsconfigPaths(),
+    vueDevTools(),
+    electron({
+      main: {
+        entry: 'electron/main.ts',
+      },
+      preload: {
+        input: 'electron/preload.ts',
+      },
+    }),
+  ],
   
   resolve: {
     alias: {
